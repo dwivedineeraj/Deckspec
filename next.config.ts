4b42ed7470a07a01c3e05c35d156ad712e6dac75
Neeraj <nee.dwi19@gmail.com>
2026-06-22 06:57:54 +0000

    feat: add custom domain compositechoice.com, remove basePath

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS === "true"
-
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: { unoptimized: true },
-  ...(isGithubActions && {
-    basePath: "/Deckspec",
-    assetPrefix: "/Deckspec/",
-  }),
 }
 
 export default nextConfig;
